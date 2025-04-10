b9b719dfc7af7501589ffa63eb2c2d424a057afc
Mayank8881 <mayankmokhere2527@gmail.com>
2025-04-11 02:51:19 +0530

    v3.4

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -12,17 +12,15 @@ let client: MongoClient;
 let clientPromise: Promise<MongoClient>;
 
 declare global {
-  // Allow global `var` declarations
-  // eslint-disable-next-line no-var
   var _mongoClientPromise: Promise<MongoClient> | undefined;
 }
 
 if (process.env.NODE_ENV === 'development') {
-  if (!global._mongoClientPromise) {
+  if (!globalThis._mongoClientPromise) {
     client = new MongoClient(uri, options);
-    global._mongoClientPromise = client.connect();
+    globalThis._mongoClientPromise = client.connect();
   }
-  clientPromise = global._mongoClientPromise;
+  clientPromise = globalThis._mongoClientPromise;
 } else {
   client = new MongoClient(uri, options);
   clientPromise = client.connect();
